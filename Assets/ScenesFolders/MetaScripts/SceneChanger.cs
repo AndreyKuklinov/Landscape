d91dd8c790a448b8f28abe6715f8c12dc59dca3b
Filipp <fil.agaponov@mail.ru>
2022-04-01 11:47:58 +0500

    Создан метод переключения между сценами

---
+++ Assets/ScenesFolders/MetaScripts/SceneChanger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ScenesFolders.MetaScripts
+{
+    public class SceneChanger
+    {
+        public void ChangeScene(int id)
+        {
+            if (id >= 1 && id <= SceneManager.sceneCountInBuildSettings)
+                SceneManager.LoadScene(id);
+            else
+                Debug.LogError("Id scene does not exist. Max of them is - " + SceneManager.sceneCountInBuildSettings +
+                               " but was - " + id);
+        }
+    }
+}
