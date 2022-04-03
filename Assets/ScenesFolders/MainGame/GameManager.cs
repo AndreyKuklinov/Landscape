5c0ff430e01e69ae94857ec35899039024a411f3
Filipp <58597315+cfouf@users.noreply.github.com>
2022-04-03 19:50:53 +0500

    GameLogic добавлена

--- Assets/ScenesFolders/MainGame/GameManager.cs
+++ Assets/ScenesFolders/MainGame/GameManager.cs
@@ -1,9 +1,58 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Drawing;
+using UnityEngine;
 
 namespace ScenesFolders.MainGame
 {
     public class GameManager : MonoBehaviour
     {
-        
+        private enum Actions
+        {
+            Place,
+            Move
+        }
+
+        private ((Point currentCoordinates, Point coordinatesBefore), Actions) previouseAction;
+
+        public TilesEnum.Tiles[] GetPossibleTiles(int diceValue)
+        {
+            if (diceValue == 6)
+                return new[]
+                {
+                    TilesEnum.Tiles.Mountain, TilesEnum.Tiles.Forest, TilesEnum.Tiles.Plain, TilesEnum.Tiles.Lake,
+                    TilesEnum.Tiles.Village
+                };
+            return new[] {(TilesEnum.Tiles) (diceValue - 1)};
+        }
+
+        public void PlaceTile(int x, int y, TilesEnum.Tiles tile)
+        {
+            previouseAction = ((new Point(x, y), Point.Empty), Actions.Place);
+            throw new NotImplementedException();
+        }
+
+        public void MoveTile(int startX, int startY, int targetX, int targetY)
+        {
+            previouseAction = ((new Point(targetX, targetY), new Point(startX, startY)), Actions.Move);
+            throw new NotImplementedException();
+        }
+
+        public void Undo()
+        {
+            if (previouseAction.Item2 == Actions.Move)
+            {
+                throw new NotImplementedException();
+            }
+
+            if (previouseAction.Item2 == Actions.Place)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsSpotFree(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ TilesEnum.cs
@@ -0,0 +1,13 @@
+﻿
+
+public static class TilesEnum
+{
+    public enum Tiles
+    {
+        Mountain,
+        Forest,
+        Plain,
+        Lake,
+        Village
+    }
+}
