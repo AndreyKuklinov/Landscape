95de85e7db64a4c44a20c8a6cf92d0169a8c5d3a
Filipp <58597315+cfouf@users.noreply.github.com>
2022-04-14 15:43:13 +0500

    Готово к мерджу с  мейн :100:

--- Assets/ScenesFolders/MainGame/GameManager.cs
+++ Assets/ScenesFolders/MainGame/GameManager.cs
@@ -146,7 +146,7 @@ namespace ScenesFolders.MainGame
         public int CountAdjacentOfType(int x, int y, TileTypes type) =>
             GetNeighbours(x, y).Count(tile => tile.Type == type);
 
-        public IEnumerable<Tile> GetNeighbours(int x, int y)
+        private IEnumerable<Tile> GetNeighbours(int x, int y)
         {
             for (var dx = -1; dx < 1; dx++)
             for (var dy = -1; dy < 1; dy++)
