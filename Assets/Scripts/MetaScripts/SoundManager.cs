6d5ed5a6d84e50b5c80f3884c2660bc43caa48e4
Filipp <58597315+cfouf@users.noreply.github.com>
2022-05-15 14:05:04 +0500

    Sound Added

---
+++ Assets/Scripts/MetaScripts/SoundManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace MetaScripts
+{
+    public class SoundManager : MonoBehaviour
+    {
+        [SerializeField] private AudioSource buttonClickSound;
+        [SerializeField] private AudioSource[] musics;
+
+        private void Start()
+        {
+            buttonClickSound.volume = 1 + PlayerPrefs.GetFloat("InterfaceVolume");
+        }
+
+        public void ChangeInterfaceVolume()
+        {
+            buttonClickSound.volume = 1 + PlayerPrefs.GetFloat("InterfaceVolume");
+        }
+        
+        public void ChangeMusicVolume()
+        {
+            foreach (var music in musics)
+            {
+                music.volume = 1 + PlayerPrefs.GetFloat("MusicVolume");
+            }
+        }
+
+        public void PlayButtonClickSound()
+        {
+            buttonClickSound.Play();
+        }
+
+        public void PlayMusic()
+        {
+            musics[0].Play();
+        }
+    }
+}
--- Assets/Scripts/Settings/SettingsManager.cs
+++ Assets/Scripts/Settings/SettingsManager.cs
@@ -1,24 +1,34 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
-namespace ScenesFolders.Settings
+namespace Settings
 {
     public class SettingsManager : MonoBehaviour
     {
+        [SerializeField] private Slider musicVolumeScroll;
+        [SerializeField] private Slider interfaceVolumeScroll;
         public string Difficulty
         {
             set => PlayerPrefs.SetString("Difficulty", value);
             get => PlayerPrefs.GetString("Difficulty");
         }
 
-        public int MusicVolume
-        {
-            set => PlayerPrefs.SetInt("MusicVolume", value);
-            get => PlayerPrefs.GetInt("MusicVolume");
-        }
+        public float MusicVolume => PlayerPrefs.GetFloat("MusicVolume");
+
         public int InterfaceVolume
         {
             set => PlayerPrefs.SetInt("InterfaceVolume", value);
             get => PlayerPrefs.GetInt("InterfaceVolume");
+        }
+
+        public void ChangeMusicVolume()
+        {
+            PlayerPrefs.SetFloat("MusicVolume", musicVolumeScroll.value); 
+        }
+        
+        public void ChangeInterfaceVolume()
+        {
+            PlayerPrefs.SetFloat("InterfaceVolume", interfaceVolumeScroll.value);
         }
 
     }
