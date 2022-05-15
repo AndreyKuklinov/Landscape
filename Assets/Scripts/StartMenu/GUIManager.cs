b2f49f94c44935b4455ad687b59086d0e91cf702
Filipp <58597315+cfouf@users.noreply.github.com>
2022-05-15 15:05:43 +0500

    Начальное меню меняет цвет в зависимости от вермени

--- Assets/Scripts/StartMenu/GUIManager.cs
+++ Assets/Scripts/StartMenu/GUIManager.cs
@@ -1,12 +1,43 @@
-﻿using UnityEngine;
+﻿using System;
+using TreeEditor;
+using UnityEditor.Sprites;
+using UnityEngine;
+using UnityEngine.U2D;
 using UnityEngine.UI;
+using Water.Scripts;
 
-public class GUIManager : MonoBehaviour
+namespace StartMenu
 {
-    public Text ScoreText;
-
-    private void Start()
+    public class GUIManager : MonoBehaviour
     {
-        ScoreText.text = PlayerPrefs.GetInt("MaxScore").ToString();
+        [SerializeField] private Text scoreText;
+        [SerializeField] private Material day;
+        [SerializeField] private Material night;
+        [SerializeField] private Material oceanMaterial;
+
+
+        private void Awake()
+        {
+            var currentDate = DateTime.Now;
+            if (currentDate.Hour > 6 && currentDate.Hour < 20)
+            {
+                RenderSettings.skybox = day;
+                oceanMaterial.color = new Color(112, 12, 133);
+            }
+            else
+            {
+                RenderSettings.skybox = night;
+                oceanMaterial.color = new Color(0, 167, 255);
+            }
+
+        }
+
+        private void Start()
+        {
+            scoreText.text = PlayerPrefs.GetInt("MaxScore").ToString();
+        
+        }
+    
+    
     }
 }
