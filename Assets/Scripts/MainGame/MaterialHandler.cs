5c31c0980df48863b8b679f58bb044e1139aa439
AndreyKuklinov <Andreydolphin@mail.ru>
2022-05-16 19:42:23 +0500

    Сделал светящийся материал

---
+++ Assets/Scripts/MainGame/MaterialHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialHandler : MonoBehaviour
+{
+    private MeshRenderer _meshRenderer;
+    
+    public void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void SetMaterial(Material material)
+    {
+        _meshRenderer.material = material;
+    }
+}
