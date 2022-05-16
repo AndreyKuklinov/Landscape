e1e9858000248b061baa4c977b3625a6038528c8
AndreyKuklinov <Andreydolphin@mail.ru>
2022-05-16 20:26:13 +0500

    Сделал из моделей тайлов префабы

--- Assets/Scripts/MainGame/MaterialHandler.cs
+++ Assets/Scripts/MainGame/MaterialHandler.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class MaterialHandler : MonoBehaviour
+namespace MainGame
 {
-    private MeshRenderer _meshRenderer;
+    public class MaterialHandler : MonoBehaviour
+    {
+        private MeshRenderer _meshRenderer;
+        private Material _defaultMaterial;
     
-    public void Start()
-    {
-        _meshRenderer = GetComponent<MeshRenderer>();
-    }
+        public void Start()
+        {
+            _meshRenderer = GetComponent<MeshRenderer>();
+            _defaultMaterial = _meshRenderer.material;
+        }
 
-    public void SetMaterial(Material material)
-    {
-        _meshRenderer.material = material;
+        public void SetMaterial(Material material)
+        {
+            _meshRenderer.material = material;
+        }
+
+        public void ResetMaterial()
+        {
+            _meshRenderer.material = _defaultMaterial;
+        }
     }
 }
