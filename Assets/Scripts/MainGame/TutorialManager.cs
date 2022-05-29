4c526c4cd67a2cea0169f0cc5714635569a676d6
AndreyKuklinov <Andreydolphin@mail.ru>
2022-05-29 15:19:52 +0500

    Заложил основу туториала

---
+++ Assets/Scripts/MainGame/TutorialManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MainGame;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialManager : MonoBehaviour
+{
+    public bool IsTutorialActive { get; private set; }
+    public TileTypes[,] Moves { get; private set; }
+    public Objective[,] Objectives { get; private set; }
+    public GameObject PopUp;
+
+    private int _stage;
+    private Text _popupText;
+
+    public void Begin()
+    {
+        IsTutorialActive = true;
+        _stage = 0;
+        _popupText = PopUp.GetComponent<Text>();
+    }
+}
