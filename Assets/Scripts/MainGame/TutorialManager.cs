91ab4b51de9225cedb9762ab13f55e3fa98bea30
AndreyKuklinov <Andreydolphin@mail.ru>
2022-05-30 18:16:42 +0500

    Слегка сменил архитектуру туториала

--- Assets/Scripts/MainGame/TutorialManager.cs
+++ Assets/Scripts/MainGame/TutorialManager.cs
@@ -7,26 +7,53 @@ using UnityEngine.UI;
 
 public class TutorialManager : MonoBehaviour
 {
+    private static readonly string[] _stages = new string[]
+    {
+        "placeTile",
+        "pickOutOfSeveralTiles",
+        "placeQuestion",
+        "readObjective",
+        "fulfillObjective",
+        "moveCamera"
+    };
+    
     public bool IsTutorialActive { get; private set; }
     public GameObject PopUp;
     public GameManager GameManager;
-
+    
+    
     private int _stage;
     private Text _popupText;
 
     public void Start()
     {
         IsTutorialActive = true;
-        _stage = 0;
         _popupText = PopUp.GetComponentInChildren<Text>();
         PopUp.SetActive(true);
-
-        GameManager.TilePlaced += ProceedToNextStage;
-        _popupText.text = "Нажми на кнопку с деревьями, чтобы поставить лес";
+        _stage = -1;
+        ProceedToNextStage();
     }
 
-    private void ProceedToNextStage(object sender, EventArgs e)
+    private void ProceedToNextStage()
     {
-        _popupText.text = "Тест удался!";
+        _stage++;
+        var stageName = _stages[_stage];
+
+        switch (stageName)
+        {
+            case "placeTile":
+                _popupText.text = "Нажми на кнопку с деревьями, чтобы поставить лес";
+                GameManager.TilePlaced += OnTilePlaced;
+                break;
+            default:
+                _popupText.text = "Туториал сломался :(";
+                break;
+        }
+    }
+
+    private void OnTilePlaced(object sender, EventArgs e)
+    {
+        GameManager.TilePlaced -= OnTilePlaced;
+        ProceedToNextStage();
     }
 }
