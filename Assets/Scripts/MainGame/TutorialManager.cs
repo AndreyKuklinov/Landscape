1c7f267c78ebe1abd2e5f8c4acc2c7e7412ad1b6
Filipp <58597315+cfouf@users.noreply.github.com>
2022-06-01 21:33:40 +0500

    Проект готов к билду

--- Assets/Scripts/MainGame/TutorialManager.cs
+++ Assets/Scripts/MainGame/TutorialManager.cs
@@ -27,64 +27,68 @@ namespace MainGame
         {
             KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.Q, KeyCode.E
         };
+
         private readonly Dictionary<KeyCode, bool> pressedCameraKeys = new Dictionary<KeyCode, bool>()
         {
-            { KeyCode.W, false },
-            { KeyCode.A, false },
-            { KeyCode.S, false },
-            { KeyCode.D, false },
-            { KeyCode.Q, false },
-            { KeyCode.E, false },
+            {KeyCode.W, false},
+            {KeyCode.A, false},
+            {KeyCode.S, false},
+            {KeyCode.D, false},
+            {KeyCode.Q, false},
+            {KeyCode.E, false},
         };
 
         private HashSet<ScoreObject> seenObjectives = new HashSet<ScoreObject>();
         private ScoreObject firstObjective;
 
-        [SerializeField]
-        [FormerlySerializedAs("PopUp")] private GameObject popUp;
-        [SerializeField]
-        [FormerlySerializedAs("GameManager")] private GameManager gameManager;
+        [SerializeField] [FormerlySerializedAs("PopUp")]
+        private GameObject popUp;
+
+        [SerializeField] [FormerlySerializedAs("GameManager")]
+        private GameManager gameManager;
+
         [SerializeField] private List<Objective> tutorialObjectives;
+        [SerializeField] private Canvas tutorialCanvas;
 
         public bool IsTutorialActive { get; private set; }
         public int[,] Moves { get; private set; }
-    
-        private int Stage;
-        private Text PopupText;
+
+        private int stage;
+        private Text popupText;
 
         public void Begin()
         {
             IsTutorialActive = true;
-            PopupText = popUp.GetComponentInChildren<Text>();
-            popUp.SetActive(true);
-            Stage = -1;
+            tutorialCanvas.gameObject.SetActive(true);
+            popupText = popUp.GetComponentInChildren<Text>();
+            stage = -1;
             ProceedToNextStage();
         }
 
         private void ProceedToNextStage()
         {
-            Stage++;
-            var currentStage = (TutorialStages)Stage;
+            stage++;
+            var currentStage = (TutorialStages) stage;
             Moves = new int[gameManager.boardWidth, gameManager.boardWidth];
             switch (currentStage)
             {
                 case TutorialStages.MoveCameraWithKeys:
-                    PopupText.text = "Подвигайте камеру с помощью клавиш W, A, S, D, а также Q и E";
+                    popupText.text = "Подвигайте камеру с помощью клавиш W, A, S, D, а также Q и E";
                     break;
                 case TutorialStages.MoveCameraWithMouse:
-                    PopupText.text = "Повращайте камеру, зажав правую кнопку мыши";
+                    popupText.text = "Повращайте камеру, зажав правую кнопку мыши";
                     break;
                 case TutorialStages.PlaceTile:
-                    PopupText.text = "В Landscape вы размещаете клетки, чтобы зарабатывать очки. " +
+                    popupText.text = "В Landscape вы размещаете клетки, чтобы зарабатывать очки. " +
                                      "Нажмите на клетку с домом, чтобы поставить деревню.";
                     gameManager.TilePlaced += OnTilePlaced;
                     Moves[2, 2] = 5;
                     gameManager.boardRenderer.DisplayPossibleMoves();
                     break;
                 case TutorialStages.PickOutOfSeveralTiles1:
-                    PopupText.text = "Есть 5 видов клеток: Гора, Лес, Степь, Озеро и Деревня. " +
+                    popupText.text = "Есть 5 видов клеток: Гора, Лес, Степь, Озеро и Деревня. " +
                                      "Каждый ход вам предлагается выбрать один из нескольких вариантов. "
-                                     +"Сделайте несколько ходов, чтобы продолжить.";
+                                     + "Сделайте несколько ходов, чтобы продолжить.";
                     gameManager.TilePlaced += OnTilePlaced;
                     Moves[0, 0] = 1;
                     Moves[0, 4] = 2;
@@ -102,36 +106,39 @@ namespace MainGame
                     Moves[4, 2] = 3;
                     break;
                 case TutorialStages.PlaceQuestion:
-                    PopupText.text = "Иногда на поле будет появляться вопросительный знак. " +
+                    popupText.text = "Иногда на поле будет появляться вопросительный знак. " +
                                      "Он означает возможность поставить любую клетку. " +
                                      "Нажмите на него и выберите тип.";
                     gameManager.TilePlaced += OnTilePlaced;
                     Moves[1, 0] = 6;
                     break;
                 case TutorialStages.ReadObjective:
-                    PopupText.text = "Вы получаете очки за выполнение целей. " +
+                    popupText.text = "Вы получаете очки за выполнение целей. " +
                                      "Наведите курсор на счётчик в левом верхнем углу, чтобы прочитать вашу цель.";
-                    gameManager.Objectives = new[] { tutorialObjectives[0] };
+                    gameManager.Objectives = new[] {tutorialObjectives[0]};
                     gameManager.guiManager.Start();
                     break;
                 case TutorialStages.FulfillObjective:
-                    PopupText.text = "Поставьте Степь, чтобы выполнить условие и получить очки.";
+                    popupText.text = "Поставьте Степь, чтобы выполнить условие и получить очки.";
                     gameManager.TilePlaced += OnTilePlaced;
                     Moves[1, 2] = 3;
                     gameManager.boardRenderer.DisplayPossibleMoves();
                     break;
                 case TutorialStages.MoreObjectives:
-                    PopupText.text = "На игру вам даются 3 цели. Прочитайте их условия.";
-                    gameManager.Objectives = new[] {
+                    popupText.text = "На игру вам даются 3 цели. Прочитайте их условия.";
+                    gameManager.Objectives = new[]
+                    {
                         tutorialObjectives[0],
-                        tutorialObjectives[1], 
-                        tutorialObjectives[2] };
+                        tutorialObjectives[1],
+                        tutorialObjectives[2]
+                    };
                     Destroy(firstObjective.gameObject);
                     gameManager.guiManager.Start();
                     break;
                 case TutorialStages.Finale:
-                    PopupText.text = "Выполняйте цели, чтобы заработать очки. Подсказка: не фокусируйтесь лишь на одной цели, " +
-                                     "а старайтесь выполнять их равномерно. Так вы получите больше очков. Удачи!";
+                    popupText.text =
+                        "Выполняйте цели, чтобы заработать очки. Подсказка: не фокусируйтесь лишь на одной цели, " +
+                        "а старайтесь выполнять их равномерно. Так вы получите больше очков. Удачи!";
                     gameManager.TilePlaced += OnTilePlaced;
                     IsTutorialActive = false;
                     gameManager.boardRenderer.DisplayPossibleMoves();
@@ -150,7 +157,7 @@ namespace MainGame
 
         private void Update()
         {
-            if((TutorialStages)Stage == TutorialStages.MoveCameraWithKeys)
+            if ((TutorialStages) stage == TutorialStages.MoveCameraWithKeys)
             {
                 var nextStage = true;
                 foreach (var key in cameraKeys)
@@ -164,18 +171,16 @@ namespace MainGame
                 if (nextStage)
                     ProceedToNextStage();
             }
-            else if ((TutorialStages)Stage == TutorialStages.MoveCameraWithMouse && Input.GetMouseButtonUp(1))
-            {
+            else if ((TutorialStages) stage == TutorialStages.MoveCameraWithMouse && Input.GetMouseButtonUp(1))
                 ProceedToNextStage();
-            }
         }
 
         public void OnObjectiveRead(object sender, EventArgs e)
         {
             if (!IsTutorialActive)
                 return;
-            var obj = (ScoreObject)sender;
-            var stage = (TutorialStages)Stage;
+            var obj = (ScoreObject) sender;
+            var stage = (TutorialStages) this.stage;
             switch (stage)
             {
                 case TutorialStages.ReadObjective:
@@ -185,7 +190,7 @@ namespace MainGame
                 case TutorialStages.MoreObjectives:
                 {
                     seenObjectives.Add(obj);
-                    if(seenObjectives.Count == 2)
+                    if (seenObjectives.Count == 2)
                         ProceedToNextStage();
                     break;
                 }
