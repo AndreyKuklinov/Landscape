a9de1c20124ec4e9949a6b232da4de2857f97983
AndreyKuklinov <Andreydolphin@mail.ru>
2022-06-16 15:47:13 +0500

    Подготовился к подсветке тайлов с очками

---
+++ Assets/Scripts/MainGame/ScoreObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace MainGame
+{
+    public class ScoreObject : MonoBehaviour
+    {
+        public Objective Objective { get; private set; }
+        public event EventHandler CursorEntered;
+        public event EventHandler CursorExited;
+
+        public void Init(Objective objective)
+            => Objective = objective;
+
+        public void OnCursorEnter()
+            => CursorEntered?.Invoke(this, EventArgs.Empty);
+
+        public void OnCursorExit()
+            => CursorExited?.Invoke(this, EventArgs.Empty);
+    }
+}
